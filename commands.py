4bca78f7d95796cd816671a0498471626e4d3e93
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-02-05 16:08:54 -0500

    alias system refactored and in place

--- commands.py
+++ commands.py
@@ -1,13 +1,24 @@
 import ConfigParser
 from utils import get_int
+import os
+
+
 config = ConfigParser.RawConfigParser()
-config.read('~/.harvconfig')
+config.read(os.path.expanduser('~/.harvconfig'))
+
+
+STATUS_TASK_FORMAT = '''{indicator}   Project:    {entry.project_name}
+    Task:       {entry.task_name}
+    ID:         {entry.id}
+    Notes:      {entry.notes}
+    Time:       {hours}:{minutes:02d}
+    '''
 
 def add(timesheet, alias, hours=False, note=False):
     today = timesheet.today
     if alias:
-        client_id = config.get(alias, 'Client')
-        task_id = config.get(alias, 'Task')
+        client_id = config.get(alias, 'client')
+        task_id = config.get(alias, 'task')
     if not alias:
         if not hours:
             hours = get_int("How many hours to enter? ")
@@ -27,10 +38,22 @@ def add(timesheet, alias, hours=False, note=False):
         task = client['tasks'][task_selection-1]
         task_id = task['id']
 
-        #set_alias()
+        set_alias(client['name'], client_id, task_id)
 
     data = {"notes": note, "project_id":client_id, "hours":hours, "task_id":task_id}
     timesheet.add(data)
     print "Added your entry."
 
+def set_alias(client, client_id, task_id):
+    print '''Would you like to store this client and project as an alias?
+    Storing as an alias lets you easily add entries to this project with:
+    harvest add <alias> <hours> <note>
+    '''
+    if raw_input("Save an alias? ").lower() in 'yes':
+        alias = raw_input("Alias word: ")
+        config.add_section(alias)
+        config.set(alias, 'client', client_id)
+        config.set(alias, 'task', task_id)
+        with open (os.path.expanduser('~/.harvconfig'), 'wb') as configfile:
+                config.write(configfile)
 
