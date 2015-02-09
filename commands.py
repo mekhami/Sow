72c63c76225a74e8cf9de667a25256bcc921e1aa
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-02-09 11:18:23 -0500

    Refactored configuration.

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-from utils import get_int
+from utils import get_int, config_write
 import os
 
 
@@ -13,53 +13,58 @@ def add(args, config, timesheet):
 
     if args['<alias>']:
         client_id = config.get(args['<alias>'], 'client')
+        client_name = config.get(args['<alias>'], 'clientname')
         task_id = config.get(['<alias>'], 'task')
+        task_name = config.get(['<alias>'], 'taskname')
         data = {"notes": args['<note>'], "project_id": client_id, "hours": args['<hours>'], "task_id": task_id}
 
     if not args['<alias>']:
+        #Get the number of hours
         if not args['<hours>']:
             hours = get_int("How many hours to enter? ")
 
+        #User selects a client from the list
         for key, value in enumerate(today['projects']):
             print key + 1, value['name']
         client_selection = int(get_int("Select a project: "))
         client = today['projects'][client_selection-1]
+        client_name = client['name']
 
+        #User selects a task from the client task list
         for key, task in enumerate(client['tasks']):
             print key + 1, task['name']
         task_selection = int(get_int("Select a task: "))
         task = client['tasks'][task_selection-1]
+        task_name = task['name']
 
+        #User adds a note to the entry
         if not args['<note>']:
-            note = raw_input("Leave a note? (optional) ")
+            note = raw_input("Leave a note (Skip with enter): ")
 
-        set_alias(config, client['name'], client['id'], task['name'], task['id'])
+        #Prompt the user to set an alias.
+        set_alias(client['name'], client['id'], task['name'], task['id'])
 
-        data = {"notes": note, "project_id":client_id, "hours":hours, "task_id":task_id}
+        data = {"notes": note, "project_id":client['id'], "hours":hours, "task_id":task['id']}
 
     timesheet.add(data)
     print "Your entry has been saved."
     print str.format(
             STATUS_TASK_FORMAT,
-            client = client['name'],
-            task = task['name'],
+            client = client_name,
+            task = task_name,
             note = note,
             hours = hours,
             indicator = '+'
         )
 
-def set_alias(config, client, client_id, task, task_id):
+def set_alias(client, client_id, task, task_id):
     print '''Would you like to store this client and project as an alias?
     Storing as an alias lets you easily add entries to this project with:
     harvest add <alias> <hours> <note>
     '''
     if raw_input("Save an alias? ").lower() in 'yes':
         alias = raw_input("Alias word: ")
-        config.add_section(alias)
-        config.set(alias, 'client', client_id)
-        config.set(alias, 'task', task_id)
-        config.set(alias, 'clientname', client)
-        config.set(alias, 'taskname', task)
-        with open (os.path.expanduser('~/.harvconfig'), 'wb') as configfile:
-            config.write(configfile)
+
+        configdata = {'client': client_id, 'task': task_id, 'clientname': client, 'taskname': task}
+        config_write(alias, configdata)
 
--- utils.py
+++ utils.py
@@ -11,36 +11,33 @@ def get_config():
     config.read(os.path.expanduser('~/.harvconfig'))
     return config
 
-def set_credentials(config):
-    print '''PyHarvest needs your username and password to authenticate to Harvest. 
+def set_credentials():
+    print '''PyHarvest needs your username and password to authenticate to Harvest.
     Your username will be stored in a config file, and password stored in your system's keyring.
     This information will not be communicated anywhere other than your file system and Harvest.'''
     username = raw_input("Username: ")
     password = getpass.getpass()
     URI = "https://" + raw_input("What is the subdomain of your Harvest application?: ") + ".harvestapp.com"
 
-    config.add_section('Harvest')
-    config.set('Harvest', 'Username', username)
-    config.set('Harvest', 'URI', URI)
+    configdata = {'Username': username, 'URI': URI}
+    config_write('Harvest', configdata)
     keyring.set_password("Harvest", username, password)
-
-    with open(os.path.expanduser('~/.harvconfig'), 'wb') as configfile:
-        config.write(configfile)
 
     return (URI, username, password)
 
-def get_credentials(config):
+def get_credentials():
+    config = get_config()
     username = config.get('Harvest', 'Username')
     password = keyring.get_password("Harvest", username)
     URI = config.get('Harvest', 'uri')
 
     return URI, username, password
 
-def get_timesheet(config):
+def get_timesheet():
     try:
-        URI, USERNAME, PASSWORD = get_credentials(config)
+        URI, USERNAME, PASSWORD = get_credentials()
     except ConfigParser.NoSectionError:
-        URI, USERNAME, PASSWORD = set_credentials(config)
+        URI, USERNAME, PASSWORD = set_credentials()
 
     harvest = Harvest(URI, USERNAME, PASSWORD)
     return harvest 
@@ -55,3 +52,12 @@ def get_int(prompt):
             print 'Invalid value!'
     return answer
 
+def config_write(section, args):
+    '''Requires section name as an argument and a dictonary with name: info pairs.
+    Writes to the ~/.harvconfig file.'''
+    config = ConfigParser.RawConfigParser()
+    config.add_section(section)
+    for key, value in args.iteritems():
+        config.set(section, key, value)
+    with open(os.path.expanduser('~/.harvconfig'), 'ab+') as configfile:
+        config.write(configfile)
