7461a31fadc02f3d3e0a2424e0e8df7a4272fb96
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-02-09 16:36:06 -0500

    restructured files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='Sow',
+      packages = find_packages(),
+      version='0.1',
+      description='Harvest Command Line App',
+      author='Lawrence Vanderpool',
+      author_email='lawrence.vanderpool@gmail.com',
+      url='http://www.github.com/mekhami/Sow',
+      install_requires=['docopt', 'keyring', 'python-harvest'],
+      entry_points={
+          'console_scripts': [
+              'sow = sow.console:main'
+            ]
+        }
+      )
