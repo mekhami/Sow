48dd79b016da811ae8911aa649b3c096aa7f3a57
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-02-10 13:52:41 -0500

    moved all requirements to requirements.txt

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='Sow',
       author='Lawrence Vanderpool',
       author_email='lawrence.vanderpool@gmail.com',
       url='http://www.github.com/mekhami/Sow',
-      install_requires=['docopt', 'keyring'],
       entry_points={
           'console_scripts': [
               'sow = sow.console:main'
